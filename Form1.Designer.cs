754dfcb269c39a80ee7289268df2bf8f366b33ae
Berkant Gunes <gunesberkant96@gmail.com>
2025-04-14 10:17:51 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace OkulProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.cmbOgr = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtOgr = new System.Windows.Forms.TextBox();
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.txtAdres = new System.Windows.Forms.TextBox();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(124, 212);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(97, 32);
+            this.btnEkle.TabIndex = 0;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(268, 212);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(97, 32);
+            this.btnGuncelle.TabIndex = 1;
+            this.btnGuncelle.Text = "Guncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(412, 212);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(97, 32);
+            this.btnSil.TabIndex = 2;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // cmbOgr
+            // 
+            this.cmbOgr.FormattingEnabled = true;
+            this.cmbOgr.Location = new System.Drawing.Point(237, 42);
+            this.cmbOgr.Name = "cmbOgr";
+            this.cmbOgr.Size = new System.Drawing.Size(206, 24);
+            this.cmbOgr.TabIndex = 3;
+            this.cmbOgr.SelectedIndexChanged += new System.EventHandler(this.cmbOgr_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(121, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ogrenci Liste";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(131, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ogrenci No";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(139, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Ad Soyad";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(163, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Adres";
+            // 
+            // txtOgr
+            // 
+            this.txtOgr.Location = new System.Drawing.Point(237, 81);
+            this.txtOgr.Name = "txtOgr";
+            this.txtOgr.Size = new System.Drawing.Size(206, 22);
+            this.txtOgr.TabIndex = 8;
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.Location = new System.Drawing.Point(237, 124);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(206, 22);
+            this.txtAdSoyad.TabIndex = 9;
+            // 
+            // txtAdres
+            // 
+            this.txtAdres.Location = new System.Drawing.Point(237, 163);
+            this.txtAdres.Name = "txtAdres";
+            this.txtAdres.Size = new System.Drawing.Size(206, 22);
+            this.txtAdres.TabIndex = 10;
+            // 
+            // dgv
+            // 
+            this.dgv.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(12, 266);
+            this.dgv.Name = "dgv";
+            this.dgv.RowHeadersWidth = 51;
+            this.dgv.RowTemplate.Height = 24;
+            this.dgv.Size = new System.Drawing.Size(776, 172);
+            this.dgv.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.txtAdres);
+            this.Controls.Add(this.txtAdSoyad);
+            this.Controls.Add(this.txtOgr);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbOgr);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnEkle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.ComboBox cmbOgr;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtOgr;
+        private System.Windows.Forms.TextBox txtAdSoyad;
+        private System.Windows.Forms.TextBox txtAdres;
+        private System.Windows.Forms.DataGridView dgv;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OkulProject
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection Con = new SqlConnection();
+            Con.ConnectionString = @"Data Source=GUNESBERKANT\SQLEXPRESS;Initial Catalog=OkulProjectDB;Integrated Security=True;Encrypt=False";
+            Con.Open();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = Con;
+            cmd.CommandText = "select * from Ogrenci";
+
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            dgv.DataSource = dt;
+
+            SqlDataReader dr;
+            dr = cmd.ExecuteReader();
+            cmbOgr.Items.Clear();
+
+            while (dr.Read())
+            {
+                cmbOgr.Items.Add(dr["ogrenciNo"].ToString());
+            }
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            SqlConnection Con = new SqlConnection();
+            Con.ConnectionString = @"Data Source=GUNESBERKANT\SQLEXPRESS;Initial Catalog=OkulProjectDB;Integrated Security=True;Encrypt=False";
+            Con.Open();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = Con;
+            cmd.CommandText = "insert into Ogrenci(ogrenciNo,adSoyad,Adres) VALUES(@ogrenciNo,@adSoyad,@adres)";
+
+            cmd.Parameters.AddWithValue("@ogrenciNo", txtOgr.Text);
+            cmd.Parameters.AddWithValue("@adSoyad", txtAdSoyad.Text);
+            cmd.Parameters.AddWithValue("@adres", txtAdres.Text);
+
+            if(cmd.ExecuteNonQuery()>0)
+            {
+                MessageBox.Show("Eklendi");
+            }
+
+            Con.Close();
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            SqlConnection Con = new SqlConnection();
+            Con.ConnectionString = @"Data Source=GUNESBERKANT\SQLEXPRESS;Initial Catalog=OkulProjectDB;Integrated Security=True;Encrypt=False";
+            Con.Open();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = Con;
+            cmd.CommandText = "update Ogrenci set ogrenciNo = @ogrenciNo, adSoyad = @adSoyad, adres = @adres where ogrenciNo = @ogrenciNoGuncel";
+
+            cmd.Parameters.AddWithValue("@ogrenciNo", txtOgr.Text);
+            cmd.Parameters.AddWithValue("@adSoyad", txtAdSoyad.Text);
+            cmd.Parameters.AddWithValue("@adres", txtAdres.Text);
+            cmd.Parameters.AddWithValue("@ogrenciNoGuncel", txtOgr.Text);
+
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Guncellendi");
+
+            Con.Close();
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            SqlConnection Con = new SqlConnection();
+            Con.ConnectionString = @"Data Source=GUNESBERKANT\SQLEXPRESS;Initial Catalog=OkulProjectDB;Integrated Security=True;Encrypt=False";
+            Con.Open();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = Con;
+            cmd.CommandText = "delete from Ogrenci where ogrenciNo=@ogrenciNo";
+
+            cmd.Parameters.AddWithValue("@ogrenciNo", cmbOgr.SelectedItem.ToString());
+
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Silindi");
+
+            Con.Close();
+        }
+
+        private void cmbOgr_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SqlConnection Con = new SqlConnection();
+            Con.ConnectionString = @"Data Source=GUNESBERKANT\SQLEXPRESS;Initial Catalog=OkulProjectDB;Integrated Security=True;Encrypt=False";
+            Con.Open();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = Con;
+            cmd.CommandText = "select * from Ogrenci where ogrenciNo = @ogrenciNo";
+
+            cmd.Parameters.AddWithValue("@ogrenciNo", cmbOgr.SelectedItem.ToString());
+
+            SqlDataReader dr;
+            dr = cmd.ExecuteReader();
+
+            while(dr.Read())
+            {
+                txtOgr.Text = dr["ogrenciNo"].ToString();
+                txtAdSoyad.Text = dr["adSoyad"].ToString();
+                txtAdres.Text = dr["adres"].ToString();
+            }
+
+            Con.Close();
+        }
+    }
+}
